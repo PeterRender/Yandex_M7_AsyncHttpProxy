7f054faf0670aa78d324b60b3ec006bf12a55054
PeterRender <webpismo@yahoo.de>
2026-04-05 06:50:20 +0000

    Дополнен класс Server
    - Метод do_accept:
      * Добавлена поддержка установки множества соединений
      * На каждом успешном соединении запускается прокси-сессия через co_spawn
    - Добавлены поясняющие комментарии по Asio
    - Сопрограмма session выводит заглушку-лог
    - Успешный тест сервера на порту 5555 через nc (из 4-х терминалов)

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,93 @@
 #include "headers.h"
 
 #include <boost/asio.hpp>
-#include <boost/asio/io_service.hpp>
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/detached.hpp>
+#include <boost/asio/io_service.hpp>
 #include <boost/asio/read_until.hpp>
 #include <boost/asio/use_awaitable.hpp>
 
-#include <string_view>
 #include <iostream>
+#include <print>
+#include <string_view>
 
-using boost::asio::io_service;
-using boost::asio::co_spawn;
 using boost::asio::async_read_until;
 using boost::asio::awaitable;
-using boost::asio::use_awaitable;
-using boost::system::error_code;
 using boost::asio::buffer;
+using boost::asio::co_spawn;
 using boost::asio::dynamic_buffer;
+using boost::asio::io_service;
 using boost::asio::transfer_at_least;
+using boost::asio::use_awaitable;
 using boost::asio::ip::tcp;
+using boost::system::error_code;
 
-
+// Разделитель между HTTP-заголовками и телом сообщения (по стандрату два CRLF)
 constexpr std::string_view delimiter = "\r\n\r\n";
 
-
-awaitable<void> session(tcp::socket client_socket, io_service& io_service)
-{
-  // code here
+// Сопрограмма, реализующая прокси-сессию для одного клиента
+awaitable<void> session(tcp::socket client_socket, io_service &io_service) {
+    std::println("The session is running for the client");  // заглушка
+    co_return;
 }
 
-class Server
-{
+// Класс HTTP прокси-сервера
+class Server {
 public:
-  Server(io_service& io_service, short port)
-    : io_service_(io_service)
-    , acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
-    , socket_(io_service)
-  {
-    do_accept();
-  }
+    // Конструктор сервера
+    Server(io_service &io_service, short port)
+        : io_service_(io_service),  // запоминаем ссылку на диспетчера ввода-вывода Asio
+          acceptor_(                // cоздаем акцептор для приема TCP-соединений (слушаем IPv4 на заданном порту)
+              io_service, tcp::endpoint(tcp::v4(), port)),
+          socket_(io_service)  // cоздаем пустой сокет для будущих подключений
+    {
+        do_accept();  // запускаем асинхронное ожидание первого клиента
+    }
 
 private:
-  void do_accept()
-  {
-    acceptor_.async_accept(socket_,
-      [this](error_code ec)
-      {
-        // code here
-      }
-    );
-  }
+    // Запускает асинхронное ожидание входящего TCP-соединения
+    void do_accept() {
+        // Запускаем асинхронный прием TCP-соединения (в сокет пишется инфо о клиенте)
+        acceptor_.async_accept(socket_, [this](error_code ec) {
+            if (!ec)  // клиент успешно подключился
+            {
+                // Запускаем прокси-сессию для обслуживания клиента (сокет передаем в сессию, сессию не ждем)
+                co_spawn(io_service_, session(std::move(socket_), io_service_), boost::asio::detached);
 
-  io_service& io_service_;
-  tcp::acceptor acceptor_;
-  tcp::socket socket_;
+            } else {  // ошибка - выводим лог
+                std::println(stderr, "Failed to accept connection: {}", ec.message());
+            }
+
+            socket_ = tcp::socket(io_service_);  // новый сокет для следующего клиента
+            do_accept();                         // ожидаем следующего клиента
+        });
+    }
+
+    io_service &io_service_;  // ссылка на диспетчер ввода-вывода Asio
+    tcp::acceptor acceptor_;  // акцептор для приема входящих TCP-соединений
+    tcp::socket socket_;      // сокет для текущего клиента
 };
 
-int main(int argc, char* argv[]) {
-  try {
-    if (argc != 2) {
-      std::cerr << "Usage: proxy_server";
-      std::cerr << " <listen_port>\n";
-      return 1;
-    }
-    io_service io_service(1);
-    Server server(io_service, std::atoi(argv[1]));
-    io_service.run();
+int main(int argc, char *argv[]) {
+    try {
+        // Проверяем, что задан TCP-порт в командной строке
+        if (argc != 2) {
+            std::cerr << "Usage: proxy_server";
+            std::cerr << " <listen_port>\n";
+            return 1;
+        }
 
-  } catch (const std::exception& e) {
-    std::cerr << "Exception: " << e.what() << std::endl;
-  }
+        // Создаем диспетчер ввода-вывода Asio (1 поток выполнения)
+        io_service io_service(1);
+
+        // Создаем сервер на заданном TCP-порту
+        Server server(io_service, std::atoi(argv[1]));
+
+        // Запускаем цикл обработки асинхронных событий Asio
+        // Здесь основной поток блокируется, пока не будут обработаны все асинхронные операции (или остановка сервера)
+        io_service.run();
+
+    } catch (const std::exception &e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
 }
